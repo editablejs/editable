c89bf77577e79e462d0a15194331435154651895
yanmao <55792257+yanmao-cc@users.noreply.github.com>
2022-07-10 22:30:40 +0800

    config eslint

--- packages/scripts/eslint-preset.js
+++ packages/scripts/eslint-preset.js
@@ -1,25 +1,3 @@
 module.exports = {
-  extends: ["react-app"],
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
-  plugins: ["@typescript-eslint", "import", "react", "prettier"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    sourceType: "module",
-    ecmaVersion: 2020,
-    ecmaFeatures: {
-      jsx: true
-    }
-  },
-  rules: {
-    "no-redeclare": [
-      "error",
-      {
-        ignoreDeclarationMerge: true
-      }
-    ]
-  }
+  extends: ["next", "prettier"]
 };
