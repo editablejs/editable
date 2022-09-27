c177c99517f6653a148a09575d6f9083e355b89b
yanmao <me@aomao.com>
2022-09-27 23:04:53 +0800

    feat: add build script for yjs-server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,30 +1,7 @@
-#!/usr/bin/env node
 /* eslint-disable turbo/no-undeclared-env-vars */
-
-import WebSocket from 'ws'
-import http from 'http'
-import { setupWSConnection } from './utils'
-const wss = new WebSocket.Server({ noServer: true })
+import { startServer } from './server'
 
 const host = process.env.HOST || 'localhost'
 const port = parseInt(process.env.PORT || '1234') || 1234
 
-const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('okay')
-})
-
-wss.on('connection', setupWSConnection)
-
-server.on('upgrade', (request, socket, head) => {
-  // You may check auth of request here..
-  // See https://github.com/websockets/ws#client-authentication
-  const handleAuth = (ws: WebSocket) => {
-    wss.emit('connection', ws, request)
-  }
-  wss.handleUpgrade(request, socket, head, handleAuth)
-})
-
-server.listen(port, host, () => {
-  console.log(`running at '${host}' on port ${port}`)
-})
+startServer(host, port)
