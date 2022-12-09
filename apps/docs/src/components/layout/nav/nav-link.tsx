758124e42143d30dc51812ca575cd2ae48219094
yanmao <me@aomao.com>
2022-12-09 10:30:22 +0800

    fix: set nav-link boarder with transparent on dark theme

--- apps/docs/src/components/layout/nav/nav-link.tsx
+++ apps/docs/src/components/layout/nav/nav-link.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { ExternalLink } from 'components/external-link'
 import NextLink from 'next/link'
-import tw from 'twin.macro'
+import tw, { css } from 'twin.macro'
 
 interface NavLinkProps {
   href: string
@@ -10,13 +10,17 @@ interface NavLinkProps {
   target?: string
 }
 
-export default function NavLink({ href, children, isActive, target }: NavLinkProps) {
-  const classes = [
-    tw`inline-flex w-full cursor-pointer items-center border-b-2 justify-center text-base leading-9 px-3 py-0.5 hover:text-link dark:hover:text-link-dark whitespace-nowrap`,
-    isActive && tw`text-link border-link dark:text-link-dark dark:border-link-dark font-bold`,
-    !isActive && tw`border-transparent`,
-  ]
+const getClasses = (isActive: boolean) => [
+  tw`inline-flex w-full cursor-pointer items-center border-b-2 justify-center text-base leading-9 px-3 py-0.5 hover:text-link dark:hover:text-link-dark whitespace-nowrap`,
+  isActive && tw`text-link border-link dark:text-link-dark dark:border-link-dark font-bold`,
+  !isActive &&
+    css`
+      border-color: transparent !important;
+    `,
+]
 
+export default function NavLink({ href, children, isActive, target }: NavLinkProps) {
+  const classes = getClasses(isActive)
   if (href.startsWith('https://')) {
     return (
       <ExternalLink href={href} css={classes} target={target}>
