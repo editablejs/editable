99325fd2be94294ecd70ee069f3ba6cdff43219e
Yi Yang <me@aomao.com>
2023-02-05 19:28:44 +0800

    fix: under the combined input method, the history plug-in stack records errors

--- packages/plugins/history/src/with-history.ts
+++ packages/plugins/history/src/with-history.ts
@@ -27,6 +27,7 @@ const defaultHotkeys: Hotkeys = {
 
 export interface HistoryOptions {
   hotkey?: Hotkeys
+  delay?: number
 }
 
 export const withHistory = <T extends Editable>(editor: T, options: HistoryOptions = {}) => {
@@ -102,32 +103,33 @@ export const withHistory = <T extends Editable>(editor: T, options: HistoryOptio
     return op.type !== 'set_selection'
   }
 
+  let changeTime = Date.now()
+  const { delay } = options
   e.apply = (op: Operation) => {
     const stack = HistoryStack.get(e)
     const { operations } = e
     const { undos } = stack
     const lastBatch = undos[undos.length - 1]
     const lastOp = lastBatch && lastBatch.operations[lastBatch.operations.length - 1]
-    const lastOpPath = (lastBatch &&
-      lastBatch.selectionBefore &&
-      lastBatch.selectionBefore.focus.path) || [0, 0]
+
     let save = HistoryEditor.isSaving(e)
     let merge = HistoryEditor.isMerging(e)
 
     if (save == null) {
       save = historyProtocol.capture(op)
     }
-    if (
-      save &&
-      (!Editable.isComposing(e) || op.type === 'remove_text' || op.type === 'remove_node')
-    ) {
+    if (save && !Editable.isComposing(e)) {
       if (merge == null) {
-        if (lastBatch == null) {
+        if (delay !== undefined) {
+          if (Date.now() - changeTime > delay) {
+            merge = false
+          } else {
+            merge = true
+          }
+          changeTime = Date.now()
+        } else if (lastBatch == null) {
           merge = false
-        } else if (
-          operations.length !== 0 &&
-          operations.every(o => o.type === 'set_selection' || Path.equals(o.path, lastOpPath))
-        ) {
+        } else if (operations.length !== 0 && shouldMerge(op, lastOp, true)) {
           merge = true
         } else {
           merge = shouldMerge(op, lastOp)
@@ -195,27 +197,15 @@ export const withHistory = <T extends Editable>(editor: T, options: HistoryOptio
 /**
  * Check whether to merge an operation into the previous operation.
  */
-
-const shouldMerge = (op: Operation, prev: Operation | undefined): boolean => {
-  if (
-    prev &&
-    op.type === 'insert_text' &&
-    prev.type === 'insert_text' &&
-    op.offset === prev.offset + prev.text.length &&
-    Path.equals(op.path, prev.path)
-  ) {
-    return true
+const shouldMerge = (op: Operation, prev: Operation | undefined, defaultMerge = false): boolean => {
+  if (prev && op.type === 'insert_text' && prev.type === 'insert_text') {
+    return op.offset === prev.offset + prev.text.length && Path.equals(op.path, prev.path)
   }
 
-  if (
-    prev &&
-    op.type === 'remove_text' &&
-    prev.type === 'remove_text' &&
-    op.offset + op.text.length === prev.offset &&
-    Path.equals(op.path, prev.path)
-  ) {
-    return true
+  if (prev && op.type === 'remove_text' && prev.type === 'remove_text') {
+    if (Path.equals(op.path, prev.path)) return op.offset + op.text.length === prev.offset
+    return op.offset === 0 && Path.equals(op.path, Path.next(prev.path))
   }
 
-  return false
+  return defaultMerge
 }
