0b2d996ae9586d500e69694cda68f29f4ce6f9f2
kailunyao <yaokailun@cvte.com>
2023-02-09 11:56:17 +0800

    fix: #81

--- packages/editor/src/plugin/selection-drawing.ts
+++ packages/editor/src/plugin/selection-drawing.ts
@@ -95,7 +95,8 @@ export const SelectionDrawing = {
     let rects: DOMRect[] = []
     if (Range.isCollapsed(range)) {
       const domRange = Editable.toDOMRange(editor, range)
-      rects = [domRange.getBoundingClientRect()]
+      const clientRects = domRange.getClientRects();
+      rects = [clientRects[clientRects.length - 1]];
     } else {
       rects = getLineRectsByRange(editor, range)
     }
