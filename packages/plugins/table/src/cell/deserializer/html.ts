24df484646636eef33d5fce8ed8bc1fad328568e
BDDC-CND1273FNG\Administrator <gw00237847@gwm.cn>
2024-02-18 08:47:25 +0800

    fix: fix copy table link issue

--- packages/plugins/table/src/cell/deserializer/html.ts
+++ packages/plugins/table/src/cell/deserializer/html.ts
@@ -22,34 +22,45 @@ export const withTableCellHTMLDeserializerTransform: HTMLDeserializerWithTransfo
       }
       const { colSpan, rowSpan } = node as HTMLTableCellElement
       // 遍历children，每个子元素再次放入到children中
-      let ifHiddenCell = false;
-      const spanArray = [];
+      let ifHiddenCell = false
+      const spanArray: number[] = []
       children.forEach(child => {
+        // 2024/01/31 10:31:42@需求ID: 产品工作站代码优化@ZhaiCongrui/GW00247400：处理有的带有 链接 的单元格，编辑时回车光标跳到下个单元格的问题
+        // child  的 type 为 link时，会有此类问题，所以单独处理
+        if (child.type === 'link') {
+          const copyChild = { ...child }
+          Reflect.ownKeys(child).forEach(i => delete child[i])
+          child.type = 'paragraph'
+          child.children = []
+          child.children[0] = copyChild
+        }
         if (child.children === undefined) {
           if (child.text.indexOf('displaynone||||||') > -1) {
-            ifHiddenCell = true;
-            const startRow = child.text.split('||||||')[1];
-            const startCol = child.text.split('||||||')[2];
-            spanArray.push(startRow - 0);
-            spanArray.push(startCol - 0);
+            ifHiddenCell = true
+            const startRow = child.text.split('||||||')[1]
+            const startCol = child.text.split('||||||')[2]
+            spanArray.push(startRow - 0)
+            spanArray.push(startCol - 0)
           }
-          const tempChild = [{...child}];
+          const tempChild = [{ ...child }]
           //把child的所有属性移除
-          Object.keys(child).forEach(key => delete child[key]);
-          child.children = tempChild;
+          Object.keys(child).forEach(key => delete child[key])
+          child.children = tempChild
         }
-      });
+      })
 
-      const cell = ifHiddenCell ? {
-        type: TABLE_CELL_KEY,
-        children,
-        span: spanArray,
-      } : {
-        type: TABLE_CELL_KEY,
-        children,
-        colspan: colSpan,
-        rowspan: rowSpan,
-      }
+      const cell = ifHiddenCell
+        ? {
+            type: TABLE_CELL_KEY,
+            children,
+            span: spanArray,
+          }
+        : {
+            type: TABLE_CELL_KEY,
+            children,
+            colspan: colSpan,
+            rowspan: rowSpan,
+          }
       return [cell]
     }
     return next(node, options)
