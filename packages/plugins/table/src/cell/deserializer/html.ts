b68d2ad93a2573d8084d6a9b7c9b2ec427ad7594
YiYang <me@aomao.com>
2024-03-01 16:59:26 +0800

    refactor: fix table

--- packages/plugins/table/src/cell/deserializer/html.ts
+++ packages/plugins/table/src/cell/deserializer/html.ts
@@ -1,18 +1,30 @@
-import { HTMLDeserializerWithTransform } from '@editablejs/deserializer/html'
-import { Descendant, isDOMHTMLElement } from '@editablejs/models'
+import {
+  HTMLDeserializerOptions,
+  HTMLDeserializerWithTransform,
+} from '@editablejs/deserializer/html'
+import { Descendant, Editor, isDOMHTMLElement, Text } from '@editablejs/models'
 import { TABLE_CELL_KEY } from '../constants'
 
-export const withTableCellHTMLDeserializerTransform: HTMLDeserializerWithTransform = (
-  next,
-  deserializer,
-) => {
+export interface TableCellHTMLDeserializerOptions extends HTMLDeserializerOptions {
+  editor: Editor
+}
+export const withTableCellHTMLDeserializerTransform: HTMLDeserializerWithTransform<
+  TableCellHTMLDeserializerOptions
+> = (next, deserializer, { editor }) => {
   return (node, options = {}) => {
     const { text } = options
     if (isDOMHTMLElement(node) && ['TD', 'TH'].includes(node.nodeName)) {
       const children: Descendant[] = []
+      let isFontWeight = false
+      if (node.nodeName === 'TH') {
+        isFontWeight = true
+      }
       for (const child of node.childNodes) {
         const content = deserializer.transform(child, {
-          text,
+          text: {
+            ...text,
+            bold: isFontWeight ? true : undefined,
+          },
           matchNewline: true,
         })
         children.push(...content)
@@ -21,43 +33,43 @@ export const withTableCellHTMLDeserializerTransform: HTMLDeserializerWithTransfo
         children.push({ children: [{ text: '' }] })
       }
       const { colSpan, rowSpan } = node as HTMLTableCellElement
-      // 遍历children，每个子元素再次放入到children中
-      let ifHiddenCell = false
-      const spanArray: number[] = []
+
+      const notBlocks: Descendant[] = []
+      const newChildren: Descendant[] = []
+
+      // 如果不是block，那么就把notBlocks里的内容放到一个新的child里
+      const appendNotBlocks = () => {
+        if (notBlocks.length > 0) {
+          const newChild = { type: 'paragraph', children: notBlocks }
+          newChildren.push(newChild)
+          notBlocks.length = 0
+        }
+      }
       children.forEach(child => {
         // 2024/01/31 10:31:42@需求ID: 产品工作站代码优化@ZhaiCongrui/GW00247400：处理有的带有 链接 的单元格，编辑时回车光标跳到下个单元格的问题
         // child  的 type 为 link时，会有此类问题，所以单独处理
-        if (child.type === 'link') {
-          const copyChild = { ...child }
-          Reflect.ownKeys(child).forEach(i => delete child[i])
-          child.type = 'paragraph'
-          child.children = []
-          child.children[0] = copyChild
-        }
-        if (child.children === undefined) {
-          if (child.text.indexOf('displaynone||||||') > -1) {
-            ifHiddenCell = true
-            const startRow = child.text.split('||||||')[1]
-            const startCol = child.text.split('||||||')[2]
-            spanArray.push(startRow - 0)
-            spanArray.push(startCol - 0)
+        if (!Editor.isBlock(editor, child)) {
+          notBlocks.push(child)
+        } else {
+          if (notBlocks.length > 0) {
+            appendNotBlocks()
           }
-          const tempChild = [{ ...child }]
-          //把child的所有属性移除
-          Object.keys(child).forEach(key => delete child[key])
-          child.children = tempChild
+          newChildren.push(child)
         }
       })
-
-      const cell = ifHiddenCell
+      if (notBlocks.length > 0) {
+        appendNotBlocks()
+      }
+      const span = node.getAttribute('span')?.split(',').map(Number)
+      const cell = span
         ? {
             type: TABLE_CELL_KEY,
-            children,
-            span: spanArray,
+            children: newChildren,
+            span,
           }
         : {
             type: TABLE_CELL_KEY,
-            children,
+            children: newChildren,
             colspan: colSpan,
             rowspan: rowSpan,
           }
